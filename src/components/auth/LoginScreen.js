3f898557b8d14eab91eeb58ab02ba2b83b4b42a6
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-10-27 21:53:13 -0500

    login style

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -4,6 +4,24 @@ export const LoginScreen = () => {
     return (
         <div>
             <h1>LoginScreen</h1>
+
+            <form>
+
+                <label for="email" >E-mail</label>
+                <input  type="text"  name="email"  placeholder="correo@dom.com"/>
+
+                <label for="password" >E-mail</label>
+                <input  type="password"  name="password"  placeholder="password"/>
+
+                <button type="submit" > Login </button>
+
+
+                <hr />
+
+                <h2>Google</h2>
+
+            </form>
+
         </div>
     )
 }
