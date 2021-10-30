d28854dfaf610a69df84ca83887e3ced1763865f
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-10-29 21:34:59 -0500

    entries styles

---
+++ src/components/journal/JournalEntry.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const JournalEntry = () => {
+    return (
+        <div className="journal__entry">
+
+            <div className="journal__entry-picture"  style={ { backgroundSize: 'cover', backgroundImage:'url(https://midu.dev/images/wallpapers/javascript-grande-horizontal-4k.png)' } }>
+
+            </div>
+            
+        </div>
+    )
+}
