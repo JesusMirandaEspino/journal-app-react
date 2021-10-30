0a236dda234a8aa2f9ddc6e064ef2bc3a3cb7b5a
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-10-29 21:49:11 -0500

    entries styles

--- src/components/journal/JournalEntry.js
+++ src/components/journal/JournalEntry.js
@@ -2,10 +2,23 @@ import React from 'react'
 
 export const JournalEntry = () => {
     return (
-        <div className="journal__entry">
+        <div className="journal__entry pointer">
 
             <div className="journal__entry-picture"  style={ { backgroundSize: 'cover', backgroundImage:'url(https://midu.dev/images/wallpapers/javascript-grande-horizontal-4k.png)' } }>
 
+            </div>
+
+            <div className="journal__entry-body" >
+
+                <p className="journal__entry-title" > Nuevo entry </p>
+                <p className="journal__entry-content" > -**************************    </p>
+
+            </div>
+
+
+            <div className="journal__entry-date-box" >
+                <span>Monday</span>
+                <h4>28</h4>
             </div>
             
         </div>
