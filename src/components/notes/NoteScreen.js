45df0e9e42be6d3e69a3fb998931abc0c67263bc
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-11-22 21:18:47 -0600

    refresh note selected

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import { NotesAppBar } from './NotesAppBar';
 import { useForm } from '../../hooks/useForm';
@@ -7,9 +7,18 @@ export const NoteScreen = () => {
 
     const { active: note } = useSelector( state => state.notes );
 
-    const  [ formValues, handleInputChange ] =  useForm( note );
+    const  [ formValues, handleInputChange, reset ] =  useForm( note );
 
     const { title, body } = formValues;
+
+    const activeId = useRef(  note.id );
+
+    useEffect(() => {
+        if( note.id !== activeId.current ){
+            reset( note );
+            activeId.current = note.id;
+        }
+    }, [ note, reset ])
 
     return (
         <div className="note__main-content" >
