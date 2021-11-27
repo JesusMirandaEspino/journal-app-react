a2c4495e2e79a23f056ca831b030fa70bddbab37
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-11-26 21:13:52 -0600

    input file

--- src/components/notes/NotesAppBar.js
+++ src/components/notes/NotesAppBar.js
@@ -11,15 +11,24 @@ export const NotesAppBar = () => {
         dispatch( startSaveNotes( active  ) );
     }
 
+    const handlePictureClick = () => {
+        document.querySelector('#fileSelector').click();
+    }
+
+    const  handleFileChange = (e) => {
+        console.log(  e.target );
+    }
 
 
     return (
         <div  className="notes__appbar">
             <span>28 de Agosto del 2021</span>
 
+            <input id="fileSelector" name="file" type="file"  style={{ display: 'none' }} onChange={ handleFileChange } />
+
             <div> 
             
-                <button className="btn" >Picture</button>
+                <button className="btn"  onClick={ handlePictureClick } >Picture</button>
 
                 <button className="btn" onClick={ handleSave }  >Save</button>
 
