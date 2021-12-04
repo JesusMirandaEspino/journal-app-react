5ecf87c553a0b6e64cad7b213d1a5d4f50e9e185
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-12-03 19:03:26 -0600

    notes actions test

--- src/tests/actions/notes.test.js
+++ src/tests/actions/notes.test.js
@@ -1,16 +1,23 @@
 
-import { startNewNote  } from '../../actions/notes';
-import configureStore from 'redux-mock-store' //ES6 modules
-const { configureStore } = require('redux-mock-store') //CommonJS
+/*jslint es6 */
+import { startNewNote } from '../../actions/notes'; //ES6 modules
+import configureStore from 'redux-mock-store'; //ES6 modules
+import thunk from 'redux-thunk';
 
-const middlewares = []
-const mockStore = configureStore(middlewares)
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+
+const store = mockStore({
+    auth: {
+        uid: 'TESTING'
+    }
+});
 
 describe('Pruebas con las acciontes con notes', () => {
 
-    test( 'Debe de crear una nueva nota con notes', () => {
+    test( 'Debe de crear una nueva nota con notes', async() => {
 
-
+        await store.dispatch( startNewNote() );
 
     });
 
