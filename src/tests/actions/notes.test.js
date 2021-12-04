28f7d10d721063f37d6416992c57e4ced1e78836
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-12-03 18:55:00 -0600

    install redux mock

---
+++ src/tests/actions/notes.test.js
@@ -0,0 +1,17 @@
+
+import { startNewNote  } from '../../actions/notes';
+import configureStore from 'redux-mock-store' //ES6 modules
+const { configureStore } = require('redux-mock-store') //CommonJS
+
+const middlewares = []
+const mockStore = configureStore(middlewares)
+
+describe('Pruebas con las acciontes con notes', () => {
+
+    test( 'Debe de crear una nueva nota con notes', () => {
+
+
+
+    });
+
+});
