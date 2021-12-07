05ad35db23cee42621597418b384b8acd7b26088
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-12-06 18:55:04 -0600

    test env var

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -7,15 +7,16 @@ import { GoogleAuthProvider } from 'firebase/auth';
 
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBomEElyPOJlMPfDFquF6C_E_qJCav9NOk",
-    authDomain: "react-journal-app-8b428.firebaseapp.com",
-    projectId: "react-journal-app-8b428",
-    storageBucket: "react-journal-app-8b428.appspot.com",
-    messagingSenderId: "571774134460",
-    appId: "1:571774134460:web:376569b2d998bc8eaa88b9"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
-
+/* data hide
 
 const firebaseConfigTesting = {
     apiKey: "AIzaSyDBHO4znF-FUFcD2DUdXaA2LqGxC2a-s9A",
@@ -35,8 +36,8 @@ if( process.env.NODE_ENV === 'test'){
     // Initialize Firebase
     initializeApp(firebaseConfig);
 }
-
-
+*/
+initializeApp(firebaseConfig);
 
 const db = getFirestore();
 
