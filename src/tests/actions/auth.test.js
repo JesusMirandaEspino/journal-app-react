5f1c83d8dcdd56de65cb3cb1ee1ca5f81ceb18e9
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-12-08 22:02:16 -0600

    auth testing

--- src/tests/actions/auth.test.js
+++ src/tests/actions/auth.test.js
@@ -1,7 +1,28 @@
+import { login, logout } from '../../actions/auth';
+import { types } from '../../types/types';
+
 describe( 'Pruebas con auth', () => {
 
-    test('', () => {
-        
+    test('Login o logout deben de crear la accion respectiva', () => {
+        const uid = '123ABC';
+        const displaName = 'Jesus';
+
+        const loginAction = login( uid, displaName );
+        const logoutAction = logout();
+
+        expect( loginAction ).toEqual({
+        type: types.login,
+                payload: {
+                    uid,
+                    displaName
+                }
+        });
+
+        expect( logoutAction ).toEqual({
+        type: types.logout,
+        }
+        );
+
     });
 
 
