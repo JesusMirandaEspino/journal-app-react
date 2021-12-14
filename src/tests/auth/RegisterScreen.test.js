c35345df7a429434e76912e077a9a7707a8c760b
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-12-13 21:43:56 -0600

    Email RegisterScreen testing

--- src/tests/auth/RegisterScreen.test.js
+++ src/tests/auth/RegisterScreen.test.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import { MemoryRouter } from 'react-router-dom';
 import  configureStore  from 'redux-mock-store';
 import  thunk  from 'redux-thunk';
+import { types } from '../../types/types';
 
 const middleware = [thunk];
 const mockstore = configureStore( middleware );
@@ -34,9 +35,36 @@ describe( 'Pruebas con <RegisterScreen />', () => {
 
     test( 'Debe de mostrarse correctamente', () => {
 
-
         expect(wrapper).toMatchSnapshot();
 
+    });
+
+
+    test( 'Debe hacer el dispatch de la accion respectiva', () => {
+
+        const emailField = wrapper.find( 'input[name="email"]' );
+
+        emailField.simulate( 'change',  {
+            target: {
+                value: '',
+                name: 'email'
+            }
+        });
+
+
+        wrapper.find( 'form' ).simulate('submit', {
+            preventDefault(){}
+        });
+
+        const actions = store.getActions();
+
+        expect( actions[0] ).toEqual({
+            type: types.uiSetError,
+            payload: 'Email is wrong'
+        });
+
+
+        // [ { type: '[UI] Set Error', payload: 'Email is wrong' } ]
 
     });
 
